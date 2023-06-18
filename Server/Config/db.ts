a3a611fd2df5ac3cd24c651aa95ecc440730984d
Namrata Savaliya <savaliyanamrata2002@gmail.com>
2023-06-18 14:57:47 -0400

    added project files for ICE5

---
+++ Server/Config/db.ts
@@ -0,0 +1,9 @@
+let localURI= 'mongodb://127.0.0.1:27017/movies';
+let remoteURI='mongodb+srv://harmitashah:HARU4278%40shah@moviecluster.ecjwuhi.mongodb.net/movies';
+let secret = 'SomeSecret';
+
+export default {
+    localURI: localURI,
+    remoteURI: remoteURI,
+    secret: secret
+}
